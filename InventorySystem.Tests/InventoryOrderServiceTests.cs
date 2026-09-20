2124fa3a912f70166d846ce769ea3f4fe08d2daa
roop3754 <roopjeetkaur23@gmail.com>
2026-09-20 15:32:16 -0500

    test: add quantity equals stock boundary test

---
+++ InventorySystem.Tests/InventoryOrderServiceTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventorySystem.Tests
+{
+    public class InventoryOrderServiceTests
+    {
+        [Fact]
+        public void ProcessOrder_ValidOrder_ReturnsSuccess()
+        {
+            var service = new InventoryOrderService();
+
+            service.AddProduct(new Product
+            {
+                Id = "P100",
+                Name = "Keyboard",
+                UnitPrice = 100m,
+                StockQuantity = 20
+            });
+
+            var result = service.ProcessOrder("P100", 5, 0.05m);
+
+            Assert.True(result.IsSuccess);
+
+
+        }
+
+
+        [Fact]
+        public void ProcessOrder_ReducesStockQuantity()
+        {
+            var service = new InventoryOrderService();
+
+            service.AddProduct(new Product
+            {
+                Id = "P100",
+                Name = "Keyboard",
+                UnitPrice = 100m,
+                StockQuantity = 20
+            });
+
+            service.ProcessOrder("P100", 5, 0m);
+
+            var product = service.GetProduct("P100");
+
+            Assert.Equal(15, product!.StockQuantity);
+        }
+    
+    [Fact]
+        public void ProcessOrder_Quantity50_AppliesTwentyPercentDiscount()
+        {
+            var service = new InventoryOrderService();
+
+            service.AddProduct(new Product
+            {
+                Id = "P100",
+                Name = "Keyboard",
+                UnitPrice = 100m,
+                StockQuantity = 100
+            });
+
+            var result = service.ProcessOrder("P100", 50, 0m);
+
+            Assert.Equal(4000m, result.TotalCost);
+        }
+    }
+}
